1144c25572d6cd1296e6d77ff075dbd32ee67299
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-26 19:50:23 +0530

    Removed the gitignore

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,10 +5,12 @@ import prisma from "./prismadb"
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  secret:process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+  
   ],
 })
