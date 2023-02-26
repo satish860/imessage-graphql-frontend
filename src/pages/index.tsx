511b2652a2831d07a0c741abb9dade5b785ccc6b
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-26 18:08:42 +0530

    Sign In with Google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { NextPageContext } from "next";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data } = useSession();
+
+  console.log("Here is Data", data);
   return (
-   <h1>Hello Home Page!!</h1>
-  )
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>SignIn</button>
+      )}
+      {data?.user?.name}
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+  return {
+    props: {
+      session
+    }
+  }
 }
