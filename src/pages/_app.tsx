69e2fe419d3cf21ac4a09966571e32664cee4ab4
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-27 17:26:14 +0530

    Added Client for Graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { themeconst } from "../chakra/theme";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/graphql/apollo";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
+    <ApolloProvider client={client}>
     <SessionProvider session={session}>
       <ChakraProvider theme={themeconst}>
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
+    </ApolloProvider>
   );
 }
