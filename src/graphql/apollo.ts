67de589a0ac144d7011c1915b7776baf12b671ec
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-27 18:02:25 +0530

    Added the backend endpoint

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -1,12 +1,11 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
 const httpLink = new HttpLink({
-  uri: `http://localhost:4000/graphql`,
+  uri: `https://localhost:7061/graphql/`,
   credentials: 'include',
 })
 
-
 export const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
+  link: httpLink,
+  cache: new InMemoryCache(),
 })
